c0015aaec974d1aca7665df29da77cf36fb158d8
Son Hayoung <sona28@haynag.ac.kr>
2022-03-31 19:49:59 +0900

    chapter7-ing

---
+++ src/chapter7/ArrayPrint.java
@@ -0,0 +1,18 @@
+package chapter7;
+
+public class ArrayPrint {
+    public static void main(String args[]){
+        ArrayPrint array=new ArrayPrint();
+        array.printSting();
+    }
+    public void printSting(){
+        System.out.println("strings="+new String[0]);
+        System.out.println("array="+new ArrayPrint[0]);
+    }
+    public void priontPrimitiveArray(){
+        System.out.println("byteArray="+new byte[1]);
+        System.out.println("byteArray="+new byte[1]);
+        System.out.println("byteArray="+new byte[1]);
+
+    }
+}
